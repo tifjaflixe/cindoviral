d50916baa874c2fa126daee1acad114f3a36b2d4
tifjaflixe <tifjaflixe@gmail.com>
2025-07-15 14:44:26 +0700

    Add files via upload

---
+++ src/utils/site.js
@@ -0,0 +1,9 @@
+export const nama = 'Cindo Viral';
+export const desk = 'Video Bokep Indo Bocil Ngentot Jilbab Smp Mama Amoy Viral Colmek Sma Cindo Tante China';
+export const url = 'https://cindoviral.pages.dev';
+export const terbit = '2025-01-27T00:01:03Z';
+
+export const google = '2t_ZTB7zrtg4RbsbzNrhPKoYhgO1SNviBNhXc-CHwX0';
+export const yandex = '93f4d501d93b2a54'
+
+export const index = '5fe4a251-2ae5-45e2-8ca1-cdffa7d7981d';
